761d4c9b9cd5301960679bd457caabb49122c216
Alan Casas <alancasasarevalo@gmail.com>
2021-01-02 10:36:13 +0100

    [FEATURE][LOGIN] * User login and mapped was added

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -2,9 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/common/environments.dart';
+import 'package:flutter_chat/models/login_model_response.dart';
+import 'package:flutter_chat/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
 class AuthenticationProvider with ChangeNotifier {
+
+  UserModel user;
+
   Future login(String email, String password) async {
     final loginBody = {'email': email, 'password': password};
 
@@ -13,6 +18,10 @@ class AuthenticationProvider with ChangeNotifier {
           'Content-Type': 'application/json'
         });
 
+    if (response.statusCode > 199 && response.statusCode < 299) {
+      final loginResponse = loginModelResponseFromJson(response.body);
+      this.user = loginResponse.user;
+    }  
     print(response);
   }
 }
